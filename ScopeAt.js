d7548440b574aab094d5e2dcdc8fd8bacb9c44ca
GeXY <18539369332@163.com>
2018-02-18 17:13:34 +0800

    后盾angular

--- ScopeAt.js
+++ ScopeAt.js
@@ -1,13 +1,23 @@
 var myModule = angular.module('MyModule', []);
 myModule.controller('MyCtrl', function ($scope) {
-    $scope.flavor = '百威';
+    $scope.ctrlFlavor = '百威';
 });
 myModule.directive("drink", function () {
     return {
-        restrict: "AE",
-        template: "<div>{{flavor}}</div>",
-        link: function (scope, element, attrs) {
-            scope.flavor = attrs.flavor;
-        }
+        restrict: "E",
+        //1
+        // scope: {
+        //     flavor: '@'  //只传递字符串
+        // },
+        // template: "<div>{{flavor}}</div>"
+        // link: function (scope, element, attrs) {
+        //     scope.flavor = attrs.flavor;
+        // }
+
+        //2
+        scope: {
+            flavor: "="
+        },
+        template: '<input type="text" ng-model="flavor" />'
     }
 });
