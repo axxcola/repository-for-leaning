1e20738bf84b58b58ad6bff2097d9b6fa0d2cddf
axxcola <605363537@qq.com>
2018-04-09 14:21:19 +0800

    ..

--- vue/sell/src/App.vue
+++ vue/sell/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="tabs">
-    <v-header></v-header>
+    <v-header v-bind:seller="seller"></v-header>
     <div class="tab">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
@@ -20,11 +20,22 @@
 
 <script>
 import header from './components/header/header.vue';
+
+const ERR_OK = 0;
+
 export default {
   data() {
     return {
       seller: {}
     };
+  },
+  created() {
+    this.$http.get('/api/seller').then((response) => {
+      response = response.body;
+      if(response.errno === ERR_OK) {
+        this.seller = response.data;
+      }
+    });
   },
   components: {
     'v-header': header
--- vue/sell/src/main.js
+++ vue/sell/src/main.js
@@ -3,12 +3,14 @@
 import Vue from 'vue';
 import App from './App';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import goods from './components/goods/goods.vue';
 import ratings from './components/ratings/ratings.vue';
 import sellers from './components/sellers/sellers.vue';
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 const routes = [
   { path: '/goods', component: goods },
