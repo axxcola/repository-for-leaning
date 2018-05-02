d745e8f7d7515096dcf74aafc2a1383158490371
axxcola <605363537@qq.com>
2018-05-02 14:33:44 +0800

    header finished

--- vue/sell/src/components/header/header.vue
+++ vue/sell/src/components/header/header.vue
@@ -17,8 +17,18 @@
     			<span class="text">{{seller.supports[0].description}}</span>
     		</div>
     	</div>
+        <div class="support-count" v-if="seller.supports">
+            <span class="count">{{seller.supports.length}}个</span>
+            <i class="icon-keyboard_arrow_right"></i>
+        </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+        <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+        <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+        <img v-bind:src="seller.avatar" width="100%" height="100%">
+    </div>
   </div>
 </template>
 
@@ -38,9 +48,11 @@ export default {
 <style rel="stylesheet/stylus" lang="stylus">
 @import "../../common/styles/mixin";
 .header
+    position: relative
     color: #fff
-    background: #000
+    background: rgba(7, 17, 27, 0.5)
     .content-wrapper
+        position: relative
         padding: 24px 12px 18px 24px
         font-size: 0
         .avatar
@@ -72,7 +84,7 @@ export default {
                 font-size: 12px
             .supports
                 .text
-                    font-size: 12px
+                    font-size: 10px
                 .icon
                     display: inline-block
                     width: 12px
@@ -90,4 +102,55 @@ export default {
                         bg-image('invoice_1')
                     &.special
                         bg-image('special_1')
+        .support-count
+            position: absolute
+            right: 12px
+            bottom: 18px
+            padding: 0 8px
+            height: 24px
+            line-height: 24px
+            border-radius: 14px
+            background: rgba(0, 0, 0, 0.2)
+            text-align: center
+            .count
+                font-size: 10px
+            .icon-keyboard_arrow_right
+                margin-left: 2px
+                line-height: 24px
+                font-size: 10px
+    .bulletin-wrapper
+        position: relative
+        height: 28px
+        line-height: 28px
+        padding: 0 22px 0 12px
+        white-space: nowrap
+        overflow: hidden
+        text-overflow: ellipsis
+        background: rgba(7, 17, 27, 0.2)
+        .bulletin-title
+            display: inline-block
+            vertical-align: top
+            margin-top: 7px
+            width: 22px
+            height: 12px
+            bg-image('bulletin')
+            background-size: 22px 12px
+            background-repeat: no-repeat
+        .bulletin-text
+            margin: 0 4px
+            font-size: 10px
+            vertical-align: top
+        .icon-keyboard_arrow_right
+            position: absolute
+            font-size: 10px
+            right: 12px
+            top: 8px
+    .background
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+        z-index: -1
+        filter: blur(10px)
 </style>
