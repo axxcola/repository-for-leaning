25e9a77d12680a8323c03dc713b5ebf0fe15d7f6
axxcola <605363537@qq.com>
2018-05-02 17:55:21 +0800

    rest for header component part 1

--- vue/sell/src/components/header/header.vue
+++ vue/sell/src/components/header/header.vue
@@ -41,6 +41,20 @@
                         <div class="line"></div>
                     </div>
                 </div>
+                <ul v-if="seller.supports" class="supports">
+                    <li class="support-item" v-for="(item, $index) in seller.supports">
+                        <span class="icon" v-bind:class="classMap[seller.supports[$index].type]"></span>
+                        <span class="text">{{seller.supports[$index].description}}</span>
+                    </li>
+                </ul>
+                <div class="title">
+                    <div class="line"></div>
+                    <div class="text">商家公告</div>
+                    <div class="line"></div>
+                </div>
+                <div class="bulletin">
+                    <p class="content">{{seller.bulletin}}</p>
+                </div>
             </div>
         </div>
         <div class="detail-close" @click="closeDetail">
@@ -225,7 +239,45 @@ export default {
                         border-bottom: 1px solid rgba(255, 255, 255, 0.2)
                     .text
                         padding: 0 12px 
+                        font-weight: 700
                         font-size: 14px
+                .supports
+                    width: 80%
+                    margin: 0 auto
+                    .support-item
+                        padding: 0 12px
+                        margin-bottom: 12px
+                        font-size: 0
+                        &.last-child
+                            margin-bottom: 0
+                        .icon
+                            display: inline-block
+                            width: 16px
+                            height: 16px
+                            vertical-align: top
+                            margin-right: 6px
+                            background-size: 16px 16px
+                            background-repeat: no-repeat
+                            &.decrease
+                                bg-image('decrease_2')
+                            &.discount
+                                bg-image('discount_2')
+                            &.guarantee
+                                bg-image('guarantee_2')
+                            &.invoice
+                                bg-image('invoice_2')
+                            &.special
+                                bg-image('special_2')
+                        .text
+                            line-height: 16px
+                            font-size: 12px
+                .bulletin
+                    width: 80%
+                    margin: 0 auto
+                    .content
+                        padding: 0 12px
+                        line-height: 24px
+                        font-size: 12px
         .detail-close
             position: relative
             width: 32px
