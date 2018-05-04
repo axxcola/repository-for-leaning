9589761919a297534be9974b6813a8896e1ba2ac
axxcola <605363537@qq.com>
2018-05-04 17:43:37 +0800

    fuck stylus

--- vue/sell/src/components/goods/goods.vue
+++ vue/sell/src/components/goods/goods.vue
@@ -1,11 +1,131 @@
 <template>
-  <div>I am goods</div>
+	<div class="goods">
+		<div class="menu-wrapper">
+			<ul>
+				<li v-for="item in goods" class="menu-item border-1px">
+					<span class="text">
+						<span v-show="item.type>0" class="icon" v-bind:class="classMap[item.type]"></span>{{item.name}}
+					</span>
+				</li>
+			</ul>
+		</div>
+		<div class="foods-wrapper">
+			<ul>
+				<li v-for="item in goods" class="food-list">
+					<h1 class="title">{{item.name}}</h1>
+					<ul>
+						<li v-for="food in item.foods" class="food-item">
+							<div class="icon">
+								<img :src="food.icon">
+							</div>
+							<div class="content">
+								<h2 class="name">{{food.name}}</h2>
+								<p class="desc">{{food.description}}</p>
+								<div class="extra">
+									<span>月售{{food.sellCount}}份</span>
+									<span>好评率{{food.rating}}%</span>
+								</div>
+								<div class="prive">
+									<span>￥{{food.price}}</span>
+									<span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
 
-<script type="text/scmascript-6">
-export default {};
+<script>
+const ERR_OK = 0;
+export default {
+	props: {
+		seller: {
+			type: Object
+		}
+	},
+	data() {
+		return {
+			goods: []
+		};
+	},
+	created() {
+		this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+		this.$http.get('/api/goods').then((response) => {
+			response = response.body;
+			if(response.errno === ERR_OK) {
+				this.goods = response.data;
+				console.log(this.goods);
+			}
+		});
+	}
+};
 </script>
 
-<style lang="stylus" type="stylesheet/stylus">
-
+<style rel="stylesheet/stylus" lang="stylus">
+@import "../../common/styles/mixin";
+.goods
+	display: flex
+	position: absolute
+	width: 100%
+	top: 174px
+	bottom: 46px
+	overflow: hidden
+	.menu-wrapper
+		flex: 0 0 80px
+		width: 80px
+		background: #f3f5f7
+		.menu-item
+			display: table
+			height: 54px
+			width: 56px
+			padding: 0 12px
+			line-height: 14px
+			.icon {
+				display: inline-block
+				vertical-align: top
+                width: 12px
+                height: 12px
+                margin-right: 2px
+                background-size: 12px 12px
+                background-repeat: no-repeat
+                &.decrease {
+                	bg-image('decrease_3')
+                }
+                &.discount {
+                	bg-image('discount_3')
+                }
+                &.guarantee {
+                    bg-image('guarantee_3')
+                }
+                &.invoice {
+                    bg-image('invoice_3')
+                }
+                &.special {
+                    bg-image('special_3')
+                }
+            }
+            .text {
+            	display: table-cell
+            	width: 56px
+            	vertical-align: middle
+            	border-1px(rgba(7, 17, 27, 0.1))
+            	font-size: 12px
+            }
+</style>
+<style rel="stylesheet/stylus" lang="stylus">
+.foods-wrapper {
+	flex: 1
+	.title {
+		padding-left: 14px
+		height: 26px
+		line-height: 26px
+		border-left: 2px solid #d9dde1
+		font-size: 12px
+		color: rgb(147, 153, 159)
+		background: #f3f5f7
+	}
+}
 </style>
