a5d7cd5eaf7a5a8f407cca4944044fae7bd03219
axxcola <605363537@qq.com>
2018-05-09 19:09:05 +0800

    shopcart component part I

--- vue/sell/src/components/goods/goods.vue
+++ vue/sell/src/components/goods/goods.vue
@@ -33,12 +33,14 @@
 				</li>
 			</ul>
 		</div>
+		<shopcart :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
 	</div>
 </template>
 
 <script>
 
 import BScroll from "better-scroll";
+import shopcart from "../shopcart/shopcart";
 
 const ERR_OK = 0;
 export default {
@@ -111,6 +113,9 @@ export default {
 			let el = foodList[index];
 			this.foodsScroll.scrollToElement(el, 300);
 		}
+	},
+	components: {
+		shopcart
 	}
 };
 </script>
